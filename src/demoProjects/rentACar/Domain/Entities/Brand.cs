b9438b69950a2c16a55fabd19e1952f511244948
Zeynel Şahin <zynl.shn@hotmail.com>
2022-08-16 08:04:33 +0300

    Initial commit

---
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand: Entity
+{
+    public string Name { get; set; }
+
+    public Brand()
+    {
+    }
+
+    public Brand(int id,string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
